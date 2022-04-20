04dc8ee6a49687fb67e1e3f8e94f189586b0502c
David Hilera <david.hilera@hotmail.com>
2022-04-19 22:17:05 -0500

    [Update] Change API

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API = 'https://platzi-store-ts-api.herokuapp.com/api/v1';
+const API = 'https://platzi-store-express-webpack.herokuapp.com/api/v1/products';
 
 const initialState = {
   cart: [],
